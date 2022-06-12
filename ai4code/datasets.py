a6581f69420aca97e7242ba78552911e71369b7a
chenglu <chenglu.she@gmail.com>
2022-06-12 23:43:06 +0800

    add v6

--- ai4code/datasets.py
+++ ai4code/datasets.py
@@ -78,6 +78,29 @@ def markdown_preprocess_v4(code):
     return code
 
 
+def no_markdown_special_v2(text):
+    # 保留顶格的 + - * >, 删除其他的
+    text = text[0] + re.sub(r"(?<!\n)[\*\+\-\>]", " ", text[1:])
+
+    # 删除 ( ) [ ] ` ~ |
+    text = re.sub(r"\(\)\[\]\{\}\<\>\~\|\`\.", " ", text)
+    return text
+
+
+def markdown_preprocess_v6(code):
+    """compare to v4:
+    1. 不删除单个字符
+    2. 保留部分顶格的特殊字符
+    3. 先删除 html tags ，再删 markdown 记号
+    """
+    code = links_to_word(code)
+    code = no_html_tags(code)
+    code = no_markdown_special_v2(code)
+    code = no_multi_spaces(code)
+    code = lemmatize(code)
+    return code
+
+
 def code_preprocess_v5(code):
     """
     仅保留顶层代码，丢弃所有 nested 的代码
@@ -107,6 +130,13 @@ def preprocessor_v5(text, type):
     return dict(
         code=code_preprocess_v5,
         markdown=markdown_preprocess_v4
+    )[type](text)
+
+
+def preprocessor_v6(text, type):
+    return dict(
+        code=code_preprocess_v4,
+        markdown=markdown_preprocess_v6
     )[type](text)
 
 
