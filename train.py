594b165b4bfac480c9b1bd940b93be81a54a9ff2
chenglu <chenglu.she@gmail.com>
2022-07-05 15:35:42 +0800

    free mem

--- train.py
+++ train.py
@@ -337,7 +337,7 @@ def main(
 
     # scheduler
     if with_scheduler:
-        iter_of_epoch = 11000 * 128 // batch_size
+        iter_of_epoch = 12000 * 128 // batch_size
         reset_fold_idx()
         scheduler = OneCycleLR(
             optimizer,
